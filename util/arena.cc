374e0e029ba98de1b2912419a2cd3ef4f1aa7ee1
xingwang218644 <xingwang218644@sohu-inc.com>
2024-10-09 14:56:42 +0800

    add comments for arena

--- util/arena.cc
+++ util/arena.cc
@@ -11,6 +11,9 @@ static const int kBlockSize = 4096;
 Arena::Arena()
     : alloc_ptr_(nullptr), alloc_bytes_remaining_(0), memory_usage_(0) {}
 
+// 源码注释
+// 析构时, 把所有new出来的内存delete掉
+// 注意使用delete[], 因为new的时候使用的是new char[]
 Arena::~Arena() {
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
@@ -18,6 +21,10 @@ Arena::~Arena() {
 }
 
 char* Arena::AllocateFallback(size_t bytes) {
+  // 源码注释
+  // 当分配的内存大于块的1/4时, 直接从操作系统分配, 不从当前block中分配
+  // 以此保证将每个block的内存碎片限制在1/4以内, 避免每个block的内存碎片过多
+  // 从OS申请一个bytes大小的block, 这个block不再用于后续分配内存, 用户单独享用
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
@@ -26,6 +33,17 @@ char* Arena::AllocateFallback(size_t bytes) {
   }
 
   // We waste the remaining space in the current block.
+  // 源码注释
+  // 只有bytes小于当前block剩余的bytes时才会调用AllocateFallback
+  // 所以此时肯定要向OS申请一个新的block, 并把新申请的block作为当前使用的block
+  // 上一个block的内存碎片浪费掉
+  // 将kBlockSize设为4K, 有以下几点好处:
+  // 减少内存碎片: Linux的内存管理里,每次内存申请都以页为单位,页的大小是4KB。
+  // 比如说从OS拿5KB的内存, 操作系统实际上会分配2页的内存也就是8KB, 这就会产生3KB的内存碎片。
+  // 而如果每次申请的内存都是4KB的整数倍, OS就会刚好分配1页, 不会产生内存碎片。
+  // 减少Page Fault的开销：4KB意味着这段内存空间位于一张页面上, 只需做1次Page Fault。
+  // 更好利用CPU缓存: 1个cache-line的大小是64B, 4KB刚好是64B的整数倍, 连续的数据块更有可能完全位于单个cache-line内。
+  // 降低Cache False-Sharing的概率
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
 
@@ -36,9 +54,20 @@ char* Arena::AllocateFallback(size_t bytes) {
 }
 
 char* Arena::AllocateAligned(size_t bytes) {
+  // 源码注释
+  // 计算对齐的长度, 最小对齐长度为8。
+  // 如果当前平台的字长大于8, 则对齐长度为字长。
+  // 确保align是2的幂次方
+  // x & (x-1) 是个位运算的技巧, 用于快速的将x的最低一位1置为0。
+  // 比如x = 0b1011, 则x & (x-1) = 0b1010。
+  // 此处用(align & (align - 1)) == 0)快速判断align是否为2的幂。
+  // 因为2的幂的二进制表示总是只有一位为1, 所以x & (x-1) == 0。
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   static_assert((align & (align - 1)) == 0,
                 "Pointer size should be a power of 2");
+  // 源码注释
+  // 位运算技巧, 等同于 current_mod = alloc_ptr_ % align
+  // 为了对齐, 多分配slop个字节
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
   size_t needed = bytes + slop;
@@ -49,6 +78,10 @@ char* Arena::AllocateAligned(size_t bytes) {
     alloc_bytes_remaining_ -= needed;
   } else {
     // AllocateFallback always returned aligned memory
+    // 源码注释
+    // AllocateFallback本身就是对齐的, 所以直接调用即可。
+    // 因为AllocateFallback要么从os直接分配,
+    // 要么new一个4KB的block, 返回block的首地址
     result = AllocateFallback(bytes);
   }
   assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0);
@@ -61,6 +94,10 @@ char* Arena::AllocateNewBlock(size_t block_bytes) {
   memory_usage_.fetch_add(block_bytes + sizeof(char*),
                           std::memory_order_relaxed);
   return result;
+
+  // 源码注释
+  // 为什么更新memory_usage_和读取memory_usage_使用的都是std::memory_order_relaxed呢？
+  // 因为memory_usage_的上下文里没有需要读取memory_usage_的地方, 不需要对指令重排做约束。
 }
 
 }  // namespace leveldb
--- util/arena.h
+++ util/arena.h
@@ -13,19 +13,40 @@
 
 namespace leveldb {
 
+// 源码注释
+// Arena是leveldb实现的简单的内存池(内存管理), 以最小4096bytes为单位申请block,
+// 使用指针记录当前block中空余内存起始位置以及当前block剩余空间。
+// 将所有的block放到blocks_数组中。
+// Arena提供了分配内存以及分配对齐的内存的两种接口,
+// 没有释放内存的接口, 当Arena的生命周期结束时, 由Arena 的析构函数统一释放内存。
+
+// 用到Arena的只有两个地方:
+// 1, MemTable::Add里使用Arena::Allocate分配代插入记录的内存
+// 2, SkipList::NewNode里使用Arena::AllocateAligned分配SkipList::Node的内存
+// MemTable::Add用于往MemTable中插入记录, 这条记录的内存即使没对齐也没关系,
+// 因为不会对这块不会像遍历数组那样挨个访问, 只是开辟一块内存把东西写进去,
+// 然后基本就不会访问这块内存了。若强行使用AllocateAligned只会徒增内存碎片。
+// 而SkipList::Node就不一样了, SkipList::Node里有个next_[]数组，next_[]会被频繁读取。
+// 如果next_[]里某个元素不是对齐的, 每次取这个元素的时候CPU都需要取两次内存, 并且会增加Cache False-Sharing的概率
 class Arena {
  public:
   Arena();
 
+  // 源码注释
+  // Arena的拷贝构造函数和赋值操作符被删除, 禁止拷贝和赋值
   Arena(const Arena&) = delete;
   Arena& operator=(const Arena&) = delete;
 
   ~Arena();
 
   // Return a pointer to a newly allocated memory block of "bytes" bytes.
+  // 源码注释
+  // 分配内存, 等同于malloc
   char* Allocate(size_t bytes);
 
   // Allocate memory with the normal alignment guarantees provided by malloc.
+  // 源码注释
+  // 分配对齐的内存, 等同于内存对齐的Allocate
   char* AllocateAligned(size_t bytes);
 
   // Returns an estimate of the total memory usage of data allocated
@@ -39,10 +60,15 @@ class Arena {
   char* AllocateNewBlock(size_t block_bytes);
 
   // Allocation state
+  // 源码注释
+  // 当前使用的block中空余内存起始位置以及当前block剩余空间
   char* alloc_ptr_;
   size_t alloc_bytes_remaining_;
 
   // Array of new[] allocated memory blocks
+  // 源码注释
+  // blocks_数组中保存了Arena申请的所有block
+  // 每次从操作系统中申请一个block, 将其添加到blocks_数组中
   std::vector<char*> blocks_;
 
   // Total memory usage of the arena.
@@ -57,12 +83,18 @@ inline char* Arena::Allocate(size_t bytes) {
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
   assert(bytes > 0);
+  // 源码注释
+  // 如果申请的bytes小于当前block剩余的bytes, 则直接从当前block中分配内存
+  // 并更新对应的指针位置以及剩余空间
   if (bytes <= alloc_bytes_remaining_) {
     char* result = alloc_ptr_;
     alloc_ptr_ += bytes;
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
+  // 源码注释
+  // 如果申请的bytes大于当前block剩余的bytes, 则重新申请一个block使用
+  // 并将新申请block添加到blocks_数组中, 当前使用的block为新申请的block
   return AllocateFallback(bytes);
 }
 
