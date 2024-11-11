0d52f61c8168fb16c2e9c723a1fd0d8723c41f9a
xingwang218644 <xingwang218644@sohu-inc.com>
2024-11-11 17:31:49 +0800

    add comments

--- helpers/memenv/memenv.h
+++ helpers/memenv/memenv.h
@@ -9,6 +9,13 @@
 
 namespace leveldb {
 
+// A pure in-memory environment
+// 源码注释
+// 提供了一个完全基于内存的文件系统接口
+// 这对于单元测试和性能分析是很有用的, 使用memenv就无需在单元测试中去做文件的创建和删除
+// 同时性能分析中也可以去除磁盘的影响, 可以方便地了解到各种操作在CPU方面的性能
+// 同时也可以与磁盘文件做性能对比分析以了解IO方面的开销。通过这个InMemoryEnv用户就可以将LevelDB架在内存
+
 class Env;
 
 // Returns a new environment that stores its data in memory and delegates
